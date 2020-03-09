b207467b3841a9a4adc63cb27c61d39316a0bfa2
William Parreira <williamsantos@intranet>
2020-03-09 16:55:13 -0300

    Inclusão da lógica de controle de view

--- src/modules/Screens/HomeScreen.js
+++ src/modules/Screens/HomeScreen.js
@@ -15,6 +15,10 @@ export default class HomeScreen extends React.Component {
     state = {
         email: '',
         displayName: '',
+        viewContasRecentes: false,
+        viewContasPendentes: false,
+        viewRelatorioMensal: false,
+        viewCadastrarConta: false
     }
 
     componentDidMount() {
@@ -42,6 +46,17 @@ export default class HomeScreen extends React.Component {
         }).start() 
     }
 
+    handleView(view: string) {
+        if(view === 'ContasRecentes')
+            this.setState({...this.state, viewContasRecentes: true, viewContasPendentes: false, viewRelatorioMensal: false, viewCadastrarConta: false})
+        if(view === 'ContasPendentes')
+            this.setState({...this.state, viewContasPendentes: true, viewContasRecentes: false, viewRelatorioMensal: false, viewCadastrarConta: false})
+        if(view === 'RelatorioMensal')
+            this.setState({...this.state, viewRelatorioMensal: true, viewContasPendentes: false, viewContasRecentes: false, viewCadastrarConta: false})
+        if(view === 'CadastrarConta')
+            this.setState({...this.state, viewCadastrarConta: true, viewContasPendentes: false, viewContasRecentes: false, viewRelatorioMensal: false})
+    }
+
     render() {
         const animatedStyle = {
             transform: [{ scale: this.animation }]
@@ -54,51 +69,75 @@ export default class HomeScreen extends React.Component {
 
                 <View style={styles.iconRow}>
                     <View style={styles.iconImg}>
-                        <Image source={require('../../assets/Icon/list.png')}></Image>
+                        <TouchableOpacity onPress={() => this.handleView('ContasRecentes')}>
+                            <Image source={require('../../assets/Icon/list.png')}></Image>
+                        </TouchableOpacity>
                     </View>
                     <View style={styles.iconImg}>
-                        <Image source={require('../../assets/Icon/boletos.png')}></Image>
+                        <TouchableOpacity onPress={() => this.handleView('ContasPendentes')}>
+                            <Image source={require('../../assets/Icon/boletos.png')}></Image>
+                        </TouchableOpacity>
                     </View>
                     <View style={styles.iconImg}>
-                        <Image source={require('../../assets/Icon/list.png')}></Image>
+                        <TouchableOpacity onPress={() => this.handleView('RelatorioMensal')}>
+                            <Image source={require('../../assets/Icon/list.png')}></Image>
+                        </TouchableOpacity>
                     </View>
                     <View style={styles.iconImg}>
-                        <Image source={require('../../assets/Icon/boletos.png')}></Image>
+                        <TouchableOpacity onPress={() => this.handleView('CadastrarConta')}>
+                            <Image source={require('../../assets/Icon/boletos.png')}></Image>
+                        </TouchableOpacity>
                     </View>
                 </View>
 
                 
-                
-                <FlatList 
-                    style={styles.contasRecentes}
-                    data={ContasParaPagarList.Contas}
-                    renderItem={({item}) => (
-                        <View style={styles.itemSection}>
-                            <Text style={styles.gridLineHeaderCell}>{item.conta}</Text>
-                            <View style={styles.itemContainer}>
-                                <View>
-                                    <Text style={styles.gridLineTextCell}>Vencimento:</Text><Text style={styles.gridLineTextCell}>{item.vencimento}</Text>
+                {
+                    this.state.viewContasRecentes ?
+                    <View>
+                        <FlatList 
+                            style={styles.contasRecentes}
+                            data={ContasParaPagarList.Contas}
+                            renderItem={({item}) => (
+                                <View style={styles.itemSection}>
+                                    <Text style={styles.gridLineHeaderCell}>{item.conta}</Text>
+                                    <View style={styles.itemContainer}>
+                                        <View style={styles.firstCell}>
+                                            <Text style={styles.gridLineTextCell}>Vencimento:</Text><Text style={styles.gridLineTextCell}>{item.vencimento}</Text>
+                                        </View>
+                                        <View>
+                                            <Text style={styles.gridLineTextCell}>Valor:</Text><Text style={styles.gridLineTextCell}>{item.valor}</Text>
+                                        </View>
+                                        <View style={styles.lastCell}>
+                                            <Image style={styles.statusIcon} source={require('../../assets/Icon/okIcon.png')}></Image>
+                                        </View>
+                                    </View>
                                 </View>
-                                <View>
-                                    <Text style={styles.gridLineTextCell}>Valor:</Text><Text style={styles.gridLineTextCell}>{item.valor}</Text>
-                                </View>
-                                <View >
-                                    <Image style={styles.statusIcon} source={require('../../assets/Icon/okIcon.png')}></Image>
-                                </View>
-                            </View>
-                        </View>
-                    )}
-                />
-                {/* <ScrollView>
-                    <View styles={styles.gridLine}>
-                        <View styles={styles.gridLineFirstCell}><Text>{ContasParaPagarList.Contas[0].conta}</Text></View>
-                        <Text styles={styles.gridLineSecondCell}>{ContasParaPagarList.Contas[0].valor}</Text>
-                        <Text styles={styles.gridLineThirdCell}>{ContasParaPagarList.Contas[0].pago}</Text>
+                            )}
+                        />
                     </View>
-                    <Image source={require('../../assets/Icon/list.png')}></Image>
-                    
-                </ScrollView> */}
-                
+                    : null
+                }
+                {
+                    this.state.viewContasPendentes ?
+                    <View>
+                        <Text>VIEW  PENDENTES</Text>
+                    </View>
+                    : null
+                }             
+                {
+                    this.state.viewRelatorioMensal ?
+                    <View>
+                        <Text>RELATÓRIO MENSAL</Text>
+                    </View>
+                    : null
+                }  
+                {
+                    this.state.viewCadastrarConta ?
+                    <View>
+                        <Text>CADASTRAR CONTA</Text>
+                    </View>
+                    : null
+                }   
 
                 {/* <BouncingButton /> */}
                 <TouchableOpacity style={styles.signOutButton} onPress={this.signOutUser}>
@@ -190,11 +229,54 @@ const styles = StyleSheet.create({
         width: 50,
         height: 50,
     },
+    firstCell: {
+        marginLeft: 5,
+    },
+    lastCell: {
+        marginRight: 5,
+        marginTop: -10,
+    },
     gridLine: {
         
     },
     gridLineHeaderCell: {
         width: 250,
         fontSize: 20,
+        marginLeft: 5,
     },
 })
+
+
+// <ScrollView style={styles.contasRecentes}>
+//     {
+//         ContasParaPagarList.Contas.map(item => {
+//             return(
+//                 <View style={styles.itemSection}>
+//                     <Text style={styles.gridLineHeaderCell}>{item.conta}</Text>
+//                     <View style={styles.itemContainer}>
+//                         <View style={styles.firstCell}>
+//                             <Text style={styles.gridLineTextCell}>Vencimento:</Text><Text style={styles.gridLineTextCell}>{item.vencimento}</Text>
+//                         </View>
+//                         <View>
+//                             <Text style={styles.gridLineTextCell}>Valor:</Text><Text style={styles.gridLineTextCell}>{item.valor}</Text>
+//                         </View>
+//                         <View style={styles.lastCell}>
+//                             <Image style={styles.statusIcon} source={require('../../assets/Icon/okIcon.png')}></Image>
+//                         </View>
+//                     </View>
+//                 </View>
+//             )
+//         })
+//     }
+// </ScrollView>
+
+
+//<ScrollView>
+//    <View styles={styles.gridLine}>
+//        <View styles={styles.gridLineFirstCell}><Text>{ContasParaPagarList.Contas[0].conta}</Text></View>
+//        <Text styles={styles.gridLineSecondCell}>{ContasParaPagarList.Contas[0].valor}</Text>
+//        <Text styles={styles.gridLineThirdCell}>{ContasParaPagarList.Contas[0].pago}</Text>
+//    </View>
+//    <Image source={require('../../assets/Icon/list.png')}></Image>
+//    
+//</ScrollView>
